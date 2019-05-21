abe4007bf63dea2d45c641cac7b912e6c1f0721a
霍然 <303176152@qq.com>
2019-05-21 16:23:03 +0800

    redux初步体验

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="jumbotron-heading text-center">{props.value}</h1>
+      <p className="text-center">
+        <button onClick={props.onIncrement} className="btn btn-primary mr-2">
+          Increase
+        </button>
+        <button onClick={props.onDecrement} className="btn btn-danger my-2">
+          Decrease
+        </button>
+      </p>
     </div>
   );
 }
+
+App.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import { createStore } from "redux";
+import reducer from "./reducers/counter";
+
+const store = createStore(reducer);
+
+// store.subscribe(() => console.log("state updated!", store.getState()));
+
+// //触发action
+// store.dispatch({
+//   type: "INCREMENT"
+// });
+
+const render = () => {
+  ReactDOM.render(
+    <App
+      onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+      onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+      value={store.getState()}
+    />,
+    document.getElementById("root")
+  );
+};
+
+render();
+
+store.subscribe(render);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
